7161aef9709485336599bedd3f84b0308b7b853e
bulywang-01 <bulywang@gmail.com>
2026-06-07 18:03:26 +0800

    Update API_BASE URL in config.js

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 // ===== Apps Script Web App URL =====
 const API_BASE =
-  'https://script.google.com/macros/s/AKfycbxKg0GZikTiPlWW2sr1kolMEJWE-rSlho6SjNTmflvIxvEyzr8ynQODxUYp2Ds_Xw_P/exec';
+  'https://script.google.com/macros/s/AKfycbxzLto17-e5hT3lwhpe19U5BL61uZLNFXs7mDY59kpk_UdV-W0CFtsDDjyjZuPU_J5o/exec';
 
 
 // ===== JSONP helper（封版唯一安全版）=====
