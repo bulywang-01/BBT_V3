6780b9f64ec9c24780e8ebb0ce49f2ccbb859fac
bulywang-01 <bulywang@gmail.com>
2026-06-09 14:56:27 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 // ===== Apps Script Web App URL =====
 const API_BASE =
-  'https://script.google.com/macros/s/AKfycbyX7657X7QhHhnmneHJ0eYSo10pb4ye60iAZ8ELI3DKVvUAIYr8fRv0VCuFVUjTipny/exec';
+  'https://script.google.com/macros/s/AKfycby45SJr44O1freZyGzaL8QSAhTHMkNI090qAoFDRYWJs9F40tE-ClzHoigC6hV4hOs/exec';
 
 
 // ===== JSONP helper（封版唯一安全版）=====
